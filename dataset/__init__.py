029de4c79ea99f4f3ebeb8b27d61147acf7589ff
In-Chang Baek <bic4907@gmail.com>
2026-03-16 15:35:54 +0900

    handle optional import of DungeonLevelDataset for compatibility with clean clones

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,12 +1,17 @@
 """Top-level package exports for project datasets."""
 
 from .multigame import GameSample, GameTag, MultiGameDataset
-from .dungeon_level_dataset.dataset import DungeonLevelDataset
 
 __all__ = [
     "MultiGameDataset",
     "GameTag",
     "GameSample",
-    "DungeonLevelDataset",
 ]
 
+# `dungeon_level_dataset` may not exist in CI/clean clones.
+try:
+    from .dungeon_level_dataset.dataset import DungeonLevelDataset
+except ModuleNotFoundError:
+    DungeonLevelDataset = None  # type: ignore[assignment]
+else:
+    __all__.append("DungeonLevelDataset")

be32885aa4aba95f6c177c12d68f01dd4c892702
In-Chang Baek <bic4907@gmail.com>
2026-03-19 14:32:58 +0900

    fix: replace dungeon_level_dataset with submodule (bic4907/dungeon-level-dataset) Remove dungeon files tracked directly in repo. Register as git submodule pointing to the official remote repo.

--- dataset/dungeon_level_dataset/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Convenience exports for dungeon-level-dataset package."""
-
-from .dataset import DungeonLevelDataset, Instruction, LevelArray
-
-__all__ = ["DungeonLevelDataset", "Instruction", "LevelArray"]
-
